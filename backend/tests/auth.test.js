179ab1fe01b14e1b4d5b0fce449118aa11faf784
shamimshihab <shamimshihab56@gmail.com>
2024-01-08 17:07:45 -0800

    update readme file

--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -1,33 +1,28 @@
-const request = require('supertest');
-const mongoose = require('mongoose');
-const app = require('../index');
+const request = require("supertest");
+const mongoose = require("mongoose");
+const app = require("../index");
+const MONGODB_URI = process.env.MONGODB_URI;
 
 beforeAll(async () => {
   // Connect to a testing database
-  await mongoose.connect('mongodb+srv://shamsulca:shamsulca2023@cluster0.l7efxwe.mongodb.net/?retryWrites=true&w=majority');
+  await mongoose.connect(MONGODB_URI);
 });
 
-// afterAll(async () => {
-//   // Disconnect from the testing database
-//   await mongoose.disconnect();
-// });
-
-
-describe('Auth Endpoints', () => {
-  it('should register a new user', async () => {
-    const res = await request(app).post('/auth/register').send({
-      username: 'testuser',
-      password: 'testpassword',
+describe("Auth Endpoints", () => {
+  it("should register a new user", async () => {
+    const res = await request(app).post("/auth/register").send({
+      username: "testuser",
+      password: "testpassword",
     });
     expect(res.statusCode).toEqual(201);
   }, 20000);
 
-  it('should log in an existing user', async () => {
-    const res = await request(app).post('/auth/login').send({
-      username: 'testuser',
-      password: 'testpassword',
+  it("should log in an existing user", async () => {
+    const res = await request(app).post("/auth/login").send({
+      username: "testuser",
+      password: "testpassword",
     });
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toHaveProperty('token');
+    expect(res.body).toHaveProperty("token");
   }, 20000);
 });
